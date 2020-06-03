79e7299a9b71bb1df1635ba96f482b587de3593f
reinaldoperes <48915319+reinaldoperes@users.noreply.github.com>
2020-06-02 22:56:20 -0300

    commit inicial do projeto

---
+++ signalR-living-square/SignalR-Project-1/MoveShapeHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+
+namespace SignalR_Project_1
+{
+    public class MoveShapeHub:Hub
+    {
+        public void UpdateModel(ShapeModel clientModel)
+        {
+            clientModel.LastUpdateBy = Context.ConnectionId;
+            Clients.AllExcept(clientModel.LastUpdateBy).updateShape(clientModel);
+        }
+
+        public class ShapeModel
+        {
+            [JsonProperty("left")]
+            public double Left { get; set; }
+            [JsonProperty("top")]
+            public double Top { get; set; }
+            [JsonIgnore]
+            public string LastUpdateBy { get; set; }
+
+        }
+    }
+}
